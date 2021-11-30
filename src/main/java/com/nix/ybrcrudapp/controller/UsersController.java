dc0c5fa97b277a54a84b71ad5d0d562565c5872d
Yaroslav Brek <yaroslav.brek@nixsolutions.com>
2021-11-30 13:51:49 +0200

    - REST controller added - GET, POST, PUT, DELETE scenarios handle added

--- src/main/java/com/nix/ybrcrudapp/controller/UsersController.java
+++ src/main/java/com/nix/ybrcrudapp/controller/UsersController.java
@@ -1,10 +1,12 @@
 package com.nix.ybrcrudapp.controller;
 
 import com.nix.ybrcrudapp.entities.User;
+import com.nix.ybrcrudapp.exception.NotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import static java.lang.String.format;
 
@@ -12,6 +14,7 @@ import static java.lang.String.format;
 @RequestMapping("user")
 public class UsersController {
     private static List<User> users;
+    private int counter = 4;
 
     public static void initUsers() {
         users = new ArrayList<User>() {{
@@ -22,19 +25,40 @@ public class UsersController {
     }
 
     @GetMapping
-    public List<User> getUsers() {
+    private List<User> getUsers() {
         return users;
     }
 
     @GetMapping("{id}")
-    public User getUser(@PathVariable String id) {
-        return users.stream().filter(item -> item.getId() == Integer.parseInt(id))
-                .findFirst().orElseThrow(() -> new IllegalArgumentException(format("The user is not found for ID: %s", id)));
+    private User getUser(@PathVariable String id) {
+        return getUserFromDb(id);
     }
 
     @DeleteMapping("{id}")
-    public String deleteUser(@PathVariable String id) {
-        users.remove(getUser(id));
-       return "";
+    private List<User> deleteUser(@PathVariable String id) {
+        users.remove(getUserFromDb(id));
+        counter--;
+        return users;
+    }
+
+    @PostMapping()
+    private List<User> createUser(@RequestBody Map<String, String> user) {
+        String key = (String) user.keySet().toArray()[0];
+        users.add(new User(counter++, key, user.get(key)));
+        return users;
+    }
+
+    @PutMapping("{id}")
+    private User createUser(@PathVariable String id, @RequestBody Map<String, String> user) {
+        User userFromDb = getUserFromDb(id);
+        String key = (String) user.keySet().toArray()[0];
+        userFromDb.setName(key);
+        userFromDb.setMessage(user.get(key));
+        return userFromDb;
+    }
+
+    private User getUserFromDb(String id) {
+        return users.stream().filter(item -> item.getId() == Integer.parseInt(id))
+                .findFirst().orElseThrow(() -> new NotFoundException(format("The user is not found for ID: %s", id)));
     }
 }
